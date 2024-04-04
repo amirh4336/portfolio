fea4efc5cc8be3a247fac95123de5847d43e8544
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-04-04 22:35:27 +0330

    feat: add dynamic active class

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
 /*==================== toggle icon navbar ====================*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 /*==================== scroll sections active link ====================*/
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 
-    /*==================== sticky navbar ====================*/
+  /*==================== sticky navbar ====================*/
+  let header = document.querySelector("header");
 
-    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
 };
 
-
 /*==================== scroll reveal ====================*/
-
 
 /*==================== typed js ====================*/
